4735ef4ce914077b8167cd6c54eacf194cb60445
ANSHU-PALIWAL <136322202+ANSHU-PALIWAL@users.noreply.github.com>
2023-10-10 12:45:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('open');
+};
+
+
+
+// back to top
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+
+
+// preloader js
+setTimeout(function () {
+    $('.loader_bg').fadeToggle();
+}, 1500);
